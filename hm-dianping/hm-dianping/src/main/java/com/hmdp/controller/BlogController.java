8fd5a93294bc16a944727c423ff1af78f140d09e
yongyuan <2629970141@qq.com>
2023-12-04 13:38:05 +0800

    推送消息与互相关注

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
@@ -33,13 +33,7 @@ public class BlogController {
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+        return blogService.saveBlog(blog);
     }
 
     @PutMapping("/like/{id}")
@@ -92,6 +86,14 @@ public class BlogController {
         List<Blog> records = page.getRecords();
         return Result.ok(records);
 
+    }
+
+
+    @GetMapping("/of/follow")
+    //传入的参数用?拼接的 传入的最后一个blog的id 即最大的 一个是偏移量 第一次来的时候为0
+    public Result queryBlogOfFollow(@RequestParam("lastId") Long max
+            , @RequestParam(value = "offset",defaultValue = "0") Integer offset){
+        return blogService.queryBlogOfFollow(max,offset);
     }
 
 }
